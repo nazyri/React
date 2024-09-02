b8aebeaeaab82fe7856e57fdd487be286ca57426
nazyri <168209590+nazyri@users.noreply.github.com>
2024-09-02 13:07:09 -0600

    react-practica

---
+++ React/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='linea'></div>
+      <div>
+            <nav className='nav'>
+            <h2><a>Inicio</a></h2>
+            <h2 className='sesion'>Iniciar Sesion</h2>
+            </nav>
+      </div>
+
+    </>
+  )
+}
+
+export default App
